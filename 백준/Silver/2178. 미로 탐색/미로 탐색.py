a0e4e047b68843d60031423353a599154b65a4ce
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2022-09-03 23:52:36 +0900

    [Silver I] Title: 미로 탐색, Time: 104 ms, Memory: 32452 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,34 +1,29 @@
-from collections import deque
+from collections import  deque
 
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-
-def bfs(start, target) : 
-    cnt = 1
-    q = deque()
-    q.append((start, cnt))
-
-    while q : 
-
-        cur, cnt = q.popleft()
-
-        if cur[1] == target[1] - 1 and cur[0] == target[0] - 1 :   
-            print(cnt)          
-            break
-
-        for i in range(4) : 
-            curX = cur[1] + dx[i]
-            curY = cur[0] + dy[i]
-
-            if (curX >= 0 and curX < target[1] and curY >= 0 and curY < target[0]) and not visited[curY][curX] and map_[curY][curX] == 1: 
-                visited[curY][curX] = True
-                q.append(((curY, curX), cnt + 1))
-
+moveX = [1, -1, 0, 0]
+moveY = [0, 0, 1, -1]
 
 n, m = map(int, input().split())
-map_ = [[int(x) for x in input()] for y in range(n)]
-
+road = [[int(x) for x in input().rstrip()] for _ in range(n)]
 visited = [[False] * m for _ in range(n)]
-visited[0][0] = True
 
-bfs((0, 0), (n, m))
+def bfs() :
+    q = deque()
+    q.append((0, 0))
+
+    while q :
+        y, x = q.popleft()
+
+        for i in range(4) :
+            dx = x + moveX[i]
+            dy = y + moveY[i]
+
+            if 0 <= dx < m and 0 <= dy < n and not visited[dy][dx] and road[dy][dx] :
+                road[dy][dx] = road[y][x] + 1
+                visited[dy][dx] = True
+
+                q.append((dy, dx))
+
+    print(road[n - 1][m - 1])
+
+bfs()
