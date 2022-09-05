2f2dbb20b55d86092881c3762ef59359ed91320b
qorwhddh2003 <58812580+qorwhddh2003@users.noreply.github.com>
2022-09-05 22:15:57 +0900

    [Silver III] Title: 2×n 타일링, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,14 +1,12 @@
 MOD = 10007
 
 n = int(input())
-dp = [-1] * (n + 1)
 
-dp[0] = 1
+dp = [0] * 1001
 dp[1] = 1
+dp[2] = 2
 
-for i in range(2, n + 1) :
-    dp[i] = dp[i - 1] + dp[i - 2]
-    dp[i] %= MOD
+for i in range(3, 1001) :
+    dp[i] = (dp[i - 1] + dp[i - 2])%MOD
 
 print(dp[n])
-
